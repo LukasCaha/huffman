c2b01dd1eee0297666fa92b13529401300e9b7aa
LukasCaha <lukas.caha@outlook.com>
2019-11-06 18:49:53 +0100

    Další testy

--- Huffman/HuffmanBuilder.cs
+++ Huffman/HuffmanBuilder.cs
@@ -7,7 +7,17 @@ namespace Huffman
 {
     public class HuffmanBuilder: IHuffmanBuilder
     {
-        public StreamReader LoadFile(string path) { return new StreamReader(path); }
+        public StreamReader LoadFile(string path)
+        {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("File Error");
+                Environment.Exit(0);
+                return null;
+            }
+
+            return new StreamReader(path);
+        }
         public int[] CountChars(StreamReader streamReader) { return new int[1]; }
         public void BuildTree(int[] charCount) { }
         public void OutputTreeInPrefix(TextWriter textWriter) { }
--- Huffman_UnitTests/UnitTest1.cs
+++ Huffman_UnitTests/UnitTest1.cs
@@ -1,5 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Huffman;
+using System;
+using System.IO;
 
 namespace Huffman_UnitTests
 {
@@ -7,10 +9,40 @@ namespace Huffman_UnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestInput()
+        public void TestFirst100CharsOfFile()
         {
-            IHuffmanBuilder builder = new HuffmanBuilder() ;
-            Assert.AreEqual(new System.IO.StreamReader("_test_files/soubor.txt").GetHashCode(),builder.LoadFile("_test_files/soubor.txt").GetHashCode());
+            string expected100Chars= "\r\naaabbabbcbabbcabbabbcbabbcabbcbabbcabbcbabbcabbcbabbcabcbabbcabbaca\r\naaabbabbcabbcabbaca\r\naaabba\r\n";
+            string actual100Chars = "";
+
+            //actual
+            IHuffmanBuilder builder = new HuffmanBuilder();
+            StreamReader actualStreamReader = builder.LoadFile("_test_files/soubor.txt");
+            using (actualStreamReader)
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    actual100Chars += (char)actualStreamReader.Read();
+                }
+            }
+
+            Assert.AreEqual((string)expected100Chars, (string)actual100Chars);
+        }
+
+        [TestMethod]
+        public void TestNonExistingFile()
+        {
+            string expectedError = "aaaaaaaaaaaaFile error\r\naaaaaaaaaaaaaaaaaa";
+
+            using (StringWriter sw = new StringWriter())
+            {
+                Console.SetOut(sw);
+                
+                IHuffmanBuilder builder = new HuffmanBuilder();
+                builder.LoadFile("_test_files/soubor_neexistujici.txt");
+
+                string result = sw.ToString();
+                Assert.AreEqual(expectedError, result);
+            }
         }
     }
 }
